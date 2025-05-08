a09bae92192f4c020bdd91be96c35c2d87767263
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-08 04:24:19 +0100

    Nice

--- src/SimpleRegistry.sol
+++ src/SimpleRegistry.sol
@@ -1,2 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+contract  SimpleRegistry {
+    mapping (address => string[]) private user;
+    string[] names;
+
+    function addName (string memory _name) public {
+        string[] memory name = user[msg.sender];
+        for (uint i = 0; i < name.length; i++) {
+           if (keccak256(bytes(name[i])) == keccak256(bytes(_name))){
+                revert("Name already Exist");
+           }
+
+           
+        }
+        user[msg.sender].push(_name);
+        
+        }
+
+    function removeName () public {}
+
+    function getAllNames () public {}
+
+
+    
+}
