c6e0c9f712aa1f35916fd944691a8bf822ecf247
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-09 00:23:17 +0100

    Forgot to push since

---
+++ src/Simple_Resgistry.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
+
+contract SimpleResgistry {
+
+    error Unauthorized();
+
+    error NameTooLong();
+
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
+    EnumerableSet.Bytes32Set private allNames;
+
+    mapping (address => EnumerableSet.Bytes32Set) private userName;
+
+    mapping(bytes32 => address) private nameToOwner;
+
+    function _stringToBytes32 (string memory _name) internal pure returns(bytes32 result) {
+        bytes memory name = bytes(_name);
+
+        if(name.length >= 32){revert NameTooLong();}
+
+        assembly {
+            result := mload(add(_name,32))
+            
+        }
+    }
+
+    function _bytes32ToString(bytes32 b32) internal pure returns (string memory) {
+        uint8 i = 0;
+        while (i < 32 && b32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (uint8 j = 0; j < i; j++) {
+            bytesArray[j] = b32[j];
+        }
+        return string(bytesArray);
+    }
+
+    function addName (string calldata _name) public {
+
+        bytes32 name = _stringToBytes32(_name);
+
+        require(allNames.contains(name) == false, "Name in use");
+
+        allNames.add(name);
+        userName[msg.sender].add(name);
+        nameToOwner[name] = msg.sender;
+
+    }
+
+    function releaseName (string calldata _name) public {
+
+        bytes32 name = _stringToBytes32(_name);
+
+        require(allNames.contains(name) == true, "Name not present");
+
+        allNames.remove(name);
+        userName[msg.sender].remove(name);
+        delete nameToOwner[name];
+
+    }
+
+    function getTheNumberOfNameInTheRegistry () public view returns (uint256){
+        return allNames.length();
+    } 
+
+    function getOwnerOfName (string calldata _name) public view returns (address) {
+
+        bytes32 name = _stringToBytes32(_name);
+
+        require(allNames.contains(name) == true, "Name not present");
+
+        return nameToOwner[name];
+    }
+
+    function getUserNames(address user) external view returns (string[] memory) {
+        uint256 len = userName[user].length();
+        string[] memory result = new string[](len);
+        for (uint256 i = 0; i < len; i++) {
+            result[i] = _bytes32ToString(userName[user].at(i));
+        }
+        return result;
+    }
+
+
+}
