2bbb383c3212dd7c660d176d9161ed9a1fe1bdf2
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-08 07:46:38 +0100

    Done

--- script/SimpleRegistry.s.sol
+++ script/SimpleRegistry.s.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import {Script} from "forge-std/Script.sol";
+import {SimpleRegistry} from "../src/SimpleRegistry.sol";
+
+contract SimpleRegisterScript is Script {
+
+    SimpleRegistry public simpleRegistry;
+    
+    function run () external {
+        
+        vm.startBroadcast();
+
+        simpleRegistry = new SimpleRegistry();
+        
+        vm.stopBroadcast();
+    }
+}
