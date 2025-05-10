4fc17fa479e5d2820ea0de2300fca0bafc38820b
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 02:48:06 +0100

    done

--- src/StringUtils.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-library StringUtils {
-    function compareStrings(string memory _a, string memory _b) internal pure returns (bool) {
-        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
-    }
-}
